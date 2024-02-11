e110f767e22c167733d91b8264c54c162218a66d
RupeshKhadka69 <khadkarupesh456@gmail.com>
2024-02-11 21:09:08 +0545

    Started backend of express-tracking-app

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express,{Request,Response} from "express";
+import dotenv from 'dotenv';
+import cors from 'cors'
+import cookieParser from"cookie-parser";
+dotenv.config();
+const app = express();
+const port =  process.env.PORT
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:false}))
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+  });
+  
+
+app.listen(port,()=> {
+    console.log(` Server is running on port ${port}`)
+})
