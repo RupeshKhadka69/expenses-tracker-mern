e9a7a7b5139af5f48dd18e781b8bfaeee3f8fb67
RupeshKhadka69 <khadkarupesh456@gmail.com>
2024-02-14 14:09:10 +0545

    backend completed for now

--- backend/src/routes/transaction.ts
+++ backend/src/routes/transaction.ts
@@ -1,10 +1,16 @@
 import express from 'express'
 import shield from '../middleware/authCheck';
 import { addIncome,getAllIncomes ,deleteIncomeById} from "../controller/income";
+import { addExpense,getAllExpenses,deleteExpenseById } from '../controller/expenses';
 const router = express.Router();
+// income routes
+
 router.post("/add-income",shield,addIncome)
 router.get("/income/all",shield,getAllIncomes);
 router.delete("/income/delete/:id",shield,deleteIncomeById);
-
+// expenses routes
+router.post("/add-expense",shield,addExpense);
+router.get("/expense/all",shield,getAllExpenses);
+router.delete("/expense/delete/:id", shield,deleteExpenseById)
 
 export default router;
